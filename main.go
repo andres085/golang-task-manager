68ab7f70d548aff2eacca6815a3b5a4e821149d3
andres085 <andresito.martinez1985@gmail.com>
2024-08-24 11:56:50 -0300

    adds handler and route path to the task create post

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -10,18 +12,30 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func taskView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific task..."))
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Display a specific task with ID %d...", id)
+	w.Write([]byte(msg))
 }
 
 func taskCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display form to create a new task..."))
 }
 
+func taskCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Save a new task..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/task/view", taskView)
-	mux.HandleFunc("/task/create", taskCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /task/view/{id}", taskView)
+	mux.HandleFunc("GET /task/create", taskCreate)
+	mux.HandleFunc("POST /task/create", taskCreatePost)
 
 	log.Print("starting server on :4000")
 
