23454f2658c368300939daf39332930affc1cdb3
andres085 <andresito.martinez1985@gmail.com>
2024-08-26 18:07:03 -0300

    add application structure, add log to it and methods to transport dependencies

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
 	files := []string{
@@ -19,19 +18,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
+		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
 		http.Error(w, "Internal Server Errror", http.StatusInternalServerError)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
+		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
 		http.Error(w, "Internal Server Errror", http.StatusInternalServerError)
 	}
 }
 
-func taskView(w http.ResponseWriter, r *http.Request) {
+func (app *application) taskView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -41,10 +40,10 @@ func taskView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific task with ID %d...", id)
 }
 
-func taskCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) taskCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new task..."))
 }
-func taskCreatePost(w http.ResponseWriter, r *http.Request) {
+func (app *application) taskCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Save a new task..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,10 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
@@ -15,16 +19,20 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /task/view/{id}", taskView)
-	mux.HandleFunc("GET /task/create", taskCreate)
-	mux.HandleFunc("POST /task/create", taskCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /task/view/{id}", app.taskView)
+	mux.HandleFunc("GET /task/create", app.taskCreate)
+	mux.HandleFunc("POST /task/create", app.taskCreatePost)
 
 	logger.Info("starting server", "addr", *addr)
 
