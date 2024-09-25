d294b6be6dda32021301ec3b07e1c2bfc96cb586
andres085 <andresito.martinez1985@gmail.com>
2024-09-25 14:25:05 -0300

    adds integration tests for user model methods

---
+++ internal/models/users_test.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/andres085/task_manager/internal/assert"
+)
+
+func TestUserInsertMethod(t *testing.T) {
+	db := newTestDB(t)
+
+	m := UserModel{db}
+	err := m.Insert("Test", "McTester", "test@mail.com", "pa$$word")
+
+	assert.NilError(t, err)
+}
+
+func TestUserAuthenticateMethod(t *testing.T) {
+	db := newTestDB(t)
+
+	email := "test@mail.com"
+	password := "pa$$word"
+
+	m := UserModel{db}
+	err := m.Insert("Test", "McTester", email, password)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	row, err := m.Authenticate(email, password)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, row, 2)
+}
